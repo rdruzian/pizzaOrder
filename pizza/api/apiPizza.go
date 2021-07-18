ec21ef12bc896480926d73aa5baa1bab15304fd2
rdruzian <rendruzian@gmail.com>
2021-07-17 21:03:21 -0300

    Correcting routers and functions

--- pizza/api/apiPizza.go
+++ pizza/api/apiPizza.go
@@ -1,12 +1,61 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"pizzaOrder/database"
+	Pizza "pizzaOrder/pizza"
+	"strconv"
+	"strings"
+)
 
 func Menu(c *gin.Context) {
-}
+	db := database.GetDatabase()
+	var pizzas []Pizza.Pizza
+	err := db.Find(&pizzas).Error
 
-func MyFlavor(c *gin.Context) {
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot find product by id: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, pizzas)
 }
 
 func NewFlavor(c *gin.Context) {
+	flavorName := c.Param("flavorName")
+	ingredientes := c.Param("ingredients")
+	preco := c.Param("price")
+	precoFloat, err := strconv.ParseFloat(preco, 64)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"eeror": "Preço precisa ser um valor numérico",
+		})
+	}
+
+	db := database.GetDatabase()
+	pizza :=  Pizza.Pizza{
+		FlavorName: flavorName,
+		Ingredients: strings.Split(ingredientes, ","),
+		Price: precoFloat,
+	}
+
+	result := db.Create(&pizza)
+	if result.Error != nil {
+		c.JSON(400, gin.H{
+			"error": "Erro ao cria sua pizza",
+		})
+	}
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot find product by id: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Sua pizza foi montado com sucesso!",
+	})
 }
--- router/router.go
+++ router/router.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"pizzaOrder/auth/authentication"
 	Customer "pizzaOrder/customer/api"
 	Order "pizzaOrder/order/api"
 	Pizza "pizzaOrder/pizza/api"
@@ -11,17 +10,17 @@ import (
 func ConfigRoutes(router *gin.Engine) *gin.Engine {
 	main := router.Group("api/v1")
 	{
-		pizza := main.Group("pizzaOrder/pizza/api", authentication.Auth())
+		pizza := main.Group("pizza")//, authentication.Auth())
 		{
 			pizza.GET("/menu", Pizza.Menu)
-			pizza.GET("/monte-sua", Pizza.MyFlavor)
-			pizza.GET("/sugestao-sabor", Pizza.NewFlavor)
+			pizza.POST("/sugestao-sabor", Pizza.NewFlavor)
 		}
 
-		order := main.Group("order", authentication.Auth())
+		order := main.Group("order")//, authentication.Auth())
 		{
-			order.GET("/", Order.History)
-			order.GET("/", Order.LastOrder)
+			order.GET("/hisotry", Order.History)
+			order.GET("/ultimo-pedido", Order.LastOrder)
+			order.POST("/ultimo-pedido", Order.NovoPedido)
 		}
 
 		customer := main.Group("customer")
