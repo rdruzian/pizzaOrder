08569c1366010935d8e878901fe76737387b54e8
rdruzian <rendruzian@gmail.com>
2021-09-02 21:56:32 -0300

    save

--- database/migrations/migrations.go
+++ database/migrations/migrations.go
@@ -3,10 +3,12 @@ package migrations
 import (
 	"gorm.io/gorm"
 	"pizzaOrder/customer"
+	"pizzaOrder/order"
+	"pizzaOrder/pizza"
 )
 
 func RunMigrations(db *gorm.DB) {
 	db.AutoMigrate(customer.Customer{})
-	//db.AutoMigrate(order.Order{})
-	//db.AutoMigrate(pizza.Pizza{})
+	db.AutoMigrate(order.Order{})
+	db.AutoMigrate(pizza.Pizza{})
 }
